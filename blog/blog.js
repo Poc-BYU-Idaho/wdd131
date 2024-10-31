53a72d9b08e80997f98d6a024e67cf88990b0235
Poc-BYU-Idaho <114012924+Poc-BYU-Idaho@users.noreply.github.com>
2024-10-30 21:28:03 -0600

    Completed build a blog part 2

--- blog/blog.js
+++ blog/blog.js
@@ -1,3 +1,4 @@
+// This array was provided with the assignment. I didn't make it. The book title and cover don't match for the 2nd book.
 const articles = [
 	{
 		id: 1,
@@ -23,5 +24,64 @@ const articles = [
 		ages: '12-16',
 		genre: 'Fantasy',
 		stars: '⭐⭐⭐⭐'
-	}
+	},
+	{
+		id: 3,
+		title: "Belgariad Book One: Pawn of Prophecy",
+		date: "Feb 12, 2022",
+		description:
+		"A fierce dispute among the Gods and the theft of a powerful Orb leaves the World divided into five kingdoms. Young Garion, with his \"Aunt Pol\" and an elderly man calling himself Wolf --a father and daughter granted near-immortality by one of the Gods -- set out on a complex mission.",
+		imgSrc:
+		"https://images-na.ssl-images-amazon.com/images/I/41ZxXA+nInL.jpg",
+		imgAlt: "Book cover for Pawn of Prophecy",
+		ages: "12-16",
+		genre: "Fantasy",
+		stars: "⭐⭐⭐⭐⭐"
+		}
 ]
+
+
+
+
+// Select the books section so we can put articles in it later.
+const booksSection = document.querySelector(".books")
+
+
+// Iterate over every article object in the articles array above.
+articles.forEach((article) => {
+	// Create an article element for each article using the createArticle function.
+	const articleElement = createArticle(article)
+
+	// Add the article element to the books section in our HTML.
+	booksSection.append(articleElement)
+})
+
+
+
+
+
+// Takes an article object, then creates and returns an HTML article element for the object.
+function createArticle(article) {
+		// Create an article element.
+		const articleElement = document.createElement("article")
+		articleElement.classList.add("book")
+	
+		// Create the content for the article element by accessing the article's properties/member variables.
+		const stringLiteral = `<div class="book-info">
+							<p>${article.date}</p>
+							<p>${article.ages}</p>
+							<p>${article.genre}</p>
+							<p>${article.stars}</p>
+						</div>
+						<div class="book-main">
+							<h2>${article.title}</h2>
+							<img src="${article.imgSrc}" alt="${article.imgAlt}">
+							<p>${article.description} <a href="">Read More...</a></p>
+						</div>`
+	
+		// Add the article content to the article element.
+		articleElement.innerHTML = stringLiteral
+
+		// Return the article element.
+		return articleElement
+}
