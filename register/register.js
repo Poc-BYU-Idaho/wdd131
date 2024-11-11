a6a3c67d8150a976c6c6b51fb6755dbc08ef6ecc
Poc-BYU-Idaho <114012924+Poc-BYU-Idaho@users.noreply.github.com>
2024-11-11 09:33:05 -0700

    Added a module file to registration form

--- register/register.js
+++ register/register.js
@@ -1,3 +1,5 @@
+import { participantTemplate, successTemplate } from "./Templates.js"
+
 let participantCount = 1
 
 document.getElementById("add").addEventListener("click", () => {
@@ -17,53 +19,6 @@ function submitForm(event) {
 }
 
 
-function participantTemplate(count) {
-    return `<section class="participant${count}">
-    <p>Participant ${count}</p>
-    <div class="item">
-      <label for="fname"> First Name<span>*</span></label>
-      <input id="fname${count}" type="text" name="fname" value="" required />
-    </div>
-    <div class="item activities">
-      <label for="activity">Activity #<span>*</span></label>
-      <input id="activity${count}" type="text" name="activity" />
-    </div>
-    <div class="item">
-      <label for="fee">Fee ($)<span>*</span></label>
-      <input id="fee${count}" type="number" name="fee" />
-    </div>
-    <div class="item">
-      <label for="date">Desired Date <span>*</span></label>
-      <input id="date${count}" type="date" name="date" />
-    </div>
-    <div class="item">
-      <p>Grade</p>
-      <select>
-        <option selected value="" disabled selected></option>
-        <option value="1">1st</option>
-        <option value="2">2nd</option>
-        <option value="3">3rd</option>
-        <option value="4">4th</option>
-        <option value="5">5th</option>
-        <option value="6">6th</option>
-        <option value="7">7th</option>
-        <option value="8">8th</option>
-        <option value="9">9th</option>
-        <option value="10">10th</option>
-        <option value="11">11th</option>
-        <option value="12">12th</option>
-      </select>
-    </div>
-  </section>`
-}
-
-
-function successTemplate(info) {
-    return `Thank you ${info.name} for registering. You have registered ${info.participantCount} participants and owe $${info.feeTotal} in fees.`
-}
-
-
-
 function totalFees() {
     // the selector below lets us grab any element that has an id that begins with "fee"
     let feeElements = document.querySelectorAll("[id^=fee]");
@@ -74,7 +29,7 @@ function totalFees() {
     feeElements = [...feeElements];
     // sum up all of the fees. Something like Array.reduce() could be very helpful here :) Or you could use a Array.forEach() as well.
     // Remember that the text that was entered into the input element will be found in the .value of the element.
-    feeTotal = feeElements.reduce((accumulator, currentValue) => {return accumulator + Number(currentValue.value)}, 0)
+    const feeTotal = feeElements.reduce((accumulator, currentValue) => {return accumulator + Number(currentValue.value)}, 0)
     // once you have your total make sure to return it!
     return feeTotal
 }
