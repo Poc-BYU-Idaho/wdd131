e5a7cefe56144c102ed400ab09afc957bc9694a5
Poc-BYU-Idaho <114012924+Poc-BYU-Idaho@users.noreply.github.com>
2024-11-20 09:26:24 -0700

    More progress on recipe book

--- arrays_practice/arrays.js
+++ arrays_practice/arrays.js
@@ -12,6 +12,8 @@ const array = [
 
 
 // Sort the array in ascending order by the id values.
+// It is sorted depending on if a negative number, a positive number, or 0 is returned.
+// I don't need to reassign my array because the sort method sorts an array in place, meaning that it changes the original array.
 array.sort((a, b) => {
     if (a.id == b.id) {
         return 0
@@ -26,10 +28,20 @@ array.sort((a, b) => {
     }
 })
 
-
 // Condensed version
 // array.sort((a, b) => {
 //     return a.id - b.id
 // })
 
+
+
+
+
+// Only keep evenly indexed items.
+// I created a new variable because filter doesn't change the original array. It just returns filtered values from an existing one.
+filteredArray = array.filter((item, index) => {
+    return index % 2 === 0
+})
+
 console.log(array)
+console.log(filteredArray)
