1a203e3ade4f9c1f058fbd1407cd6ec7078c6bdd
Poc-BYU-Idaho <114012924+Poc-BYU-Idaho@users.noreply.github.com>
2024-12-14 14:43:20 -0700

    Improved code readability

--- resistance_archive/exercise.js
+++ resistance_archive/exercise.js
@@ -4,14 +4,19 @@ import exercises from "./exercises.mjs"
 
 
 
-// Get the exercise's ID from the URL.
-const exerciseId = parseInt(new URLSearchParams(window.location.search).get('id'))
+/** Gets the exercise ID from the URL and returns the corresponding exercise from the exercise list. */
+function getExerciseFromURL() {
 
-// Retrieve the corresponding exercise from the exercise list.
-const exercise = exercises.find(exercise => exercise.id == exerciseId)
+    // Get the exercise's ID from the URL.
+    const exerciseId = parseInt(new URLSearchParams(window.location.search).get('id'))
+
+    // Retrieve and return the corresponding exercise from the exercise list.
+    return exercises.find(exercise => exercise.id == exerciseId)
+}
+
 
 /** Loads the exercise's information onto the page. */
-function loadExerciseOntoPage() {
+function loadExerciseOntoPage(exercise) {
     document.querySelector("title").innerHTML = exercise.name
     document.getElementById("exerciseName").innerHTML = exercise.name
     document.querySelector("iframe").src = exercise.videoLink
@@ -21,4 +26,8 @@ function loadExerciseOntoPage() {
 
 
 
-loadExerciseOntoPage()
+
+
+// Get the exercise and load it onto the page.
+const exercise = getExerciseFromURL()
+loadExerciseOntoPage(exercise)
--- resistance_archive/main.js
+++ resistance_archive/main.js
@@ -4,10 +4,20 @@ import exercises from "./exercises.mjs"
 
 
 
-/** Loads all the exercises into the search results. */
-function initialPageLoad() {
-    exercises.forEach(exercise => searchResultsList.innerHTML += createExerciseLink(exercise))
-}
+// Get the search results list so we can add to it.
+const searchResultsList = document.getElementById("searchResultsList")
+
+// Get the search results box so we can reference it without repeatedly having to grab it from the DOM.
+const searchInputBox = document.getElementById("searchInputBox")
+
+// Get the checkboxes so we can check which boxes are checked without having to retrieve them from the DOM every time.
+const checkboxes = document.querySelectorAll('#muscleCheckboxes input[type="checkbox"]')
+
+// Create a set of checked (in the checkboxes) muscles that we can reference against an exercise's targeted muscles in our search.
+const checkedMuscles = new Set()
+
+
+
 
 
 /** Creates a link for a certain exercise. */
@@ -16,16 +26,44 @@ function createExerciseLink(exercise) {
 }
 
 
+/** Loads all the exercises into the search results. */
+function initialPageLoad() {
+    exercises.forEach(exercise => searchResultsList.innerHTML += createExerciseLink(exercise))
+}
 
 
+/** Updates the search results based off of the search query and selected muscles */
+function updateSearchResults() {
 
-// Get the search results list so we can add to it.
-const searchResultsList = document.getElementById("searchResultsList")
+    // Get the text in the search box.
+    const searchQuery = searchInputBox.value.toLowerCase()
 
-// Get the search results box so we can reference it without repeatedly having to grab it from the DOM.
-const searchInputBox = document.getElementById("searchInputBox")
+    // Erase the current exercises from the search box so the list doesn't grow every time the search query changes.
+    searchResultsList.innerHTML = ""
 
-const checkboxes = document.querySelectorAll('#muscleCheckboxes input[type="checkbox"]')
+    // Add every exercise whose name contains the search query and targets the checked muscles (if they're checked at all).
+    exercises.forEach(exercise => {
+        if (exercise.name.toLowerCase().includes(searchQuery) && (exercise.targetedMuscles.some(targetedMuscle => checkedMuscles.has(targetedMuscle.toLowerCase())) || checkedMuscles.size == 0)) {
+
+            searchResultsList.innerHTML += createExerciseLink(exercise)
+        }
+    })
+}
+
+
+/** Updates the set of checked muscles so each exercise's targeted muscles can be referenced against it.  */
+function updateCheckboxMuscles() {
+
+    // Empty the set so previously checked muscles are removed.
+    checkedMuscles.clear()
+
+    // Add the checked muscles to the set.
+    checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            checkedMuscles.add(checkbox.id)
+        }
+    })
+}
 
 
 
@@ -37,33 +75,12 @@ initialPageLoad()
 
 
 
-
-
-// Create a set of checked (in the checkboxes) muscles that we can reference against an exercise's targeted muscles in our search.
-let checkedMuscles = new Set()
-
-
-
-
 // Add an event listener to the search box that filters the exercsies as the user searches for them.
 searchInputBox.addEventListener("input", () => {
 
-    // Get the text in the search box.
-    const searchQuery = searchInputBox.value.toLowerCase()
-
-    // Erase the current exercises from the search box so the list doesn't grow every time the search query changes.
-    searchResultsList.innerHTML = ""
-
-    // Add every exercise whose name contains the search query and targets the checked muscles (if they're checked at all).
-    exercises.forEach(exercise => {
-        if (exercise.name.toLowerCase().includes(searchQuery) && (exercise.targetedMuscles.some(targetedMuscle => checkedMuscles.has(targetedMuscle.toLowerCase())) || checkedMuscles.size == 0)) {
-
-            searchResultsList.innerHTML += createExerciseLink(exercise)
-        }
-    })
+    // Update the search results.
+    updateSearchResults()
 })
-
-
 
 
 
@@ -71,27 +88,9 @@ searchInputBox.addEventListener("input", () => {
 // Add an event listener to every checkbox that does the same thing as the event listener above, but also updates the checkedMuscles set.
 checkboxes.forEach(checkbox => checkbox.addEventListener("change", () => {
 
-    // Empty the set so previously checked muscles are removed.
-    checkedMuscles.clear()
+    // Update the set of checked muscles so our search results can be correct.
+    updateCheckboxMuscles()
 
-    // Add the checked muscles to the set.
-    checkboxes.forEach(checkbox => {
-        if (checkbox.checked) {
-            checkedMuscles.add(checkbox.id)
-        }
-    })
-
-    // Get the text in the search box.
-    const searchQuery = searchInputBox.value.toLowerCase()
-
-    // Erase the current exercises from the search box so the list doesn't grow every time the search query changes.
-    searchResultsList.innerHTML = ""
-
-    // Add every exercise whose name contains the search query and targets the checked muscles (if they're checked at all).
-    exercises.forEach(exercise => {
-        if (exercise.name.toLowerCase().includes(searchQuery) && (exercise.targetedMuscles.some(targetedMuscle => checkedMuscles.has(targetedMuscle.toLowerCase())) || checkedMuscles.size == 0)) {
-
-            searchResultsList.innerHTML += createExerciseLink(exercise)
-        }
-    })
+    // Update the search results.
+    updateSearchResults()
 }))
